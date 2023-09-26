ae0662e3e1253b17747fdd7d2149af5852b8389f
President-Asher <Asher.renzenbrink@gmail.com>
2023-09-26 09:25:01 -0400

    Ver. 1.1

--- python_fundamentals_exam.py
+++ python_fundamentals_exam.py
@@ -1,2 +1,12 @@
 intro = "Hello World"
 print(intro)
+
+first_name = input("First Name: ")
+print(first_name)
+
+age = input("Age: ")
+age = int
+
+temperature = input("Temperature: ")
+temperature = float
+
